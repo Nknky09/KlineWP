ac8ff1798da5695605df55fa76f57811a4f8cbf9
Nknky09 <ks.shido@gmail.com>
2025-03-18 18:59:51 -0400

    new front page images

--- page-second.php
+++ page-second.php
@@ -3,7 +3,7 @@
 <content>
         <div class="content-banner-two">
           <img class="bg-image" src="<?php echo get_template_directory_uri(); ?>/img/Untitled2.png"/>
-           <div class="home-logo">
+           <div class="home-logo2">
             <img class="bg-logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" />
           </div>
         </div>
@@ -119,24 +119,10 @@
             <div class="container-locations">
               <div id="map"></div>
               <div class="sidebar-map"> 
-                <h3>USA Locations</h3>
-                  <div class="sidebar-map-wrapper">
+                <input type="text" id="map-location-search" class="map-location-search" placeholder="Search Location">
+                  <div class="sidebar-map-wrapper" id="sidebar-map-wrapper">
                   
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(0);" >New York</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(1);" >Los Angeles</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(2);" >Chicago</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(3);" >Atlanta</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(4);" >Dallas</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(5);" >San Fransisco</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(6);" >Seattle</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(7);" >McAllen</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(8);" >San Diego</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(9);" >Phoneix</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(10);" >Columbus</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(11);" >Lexington</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(12);" >Lincoln</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(13);" >Charlotte</a></span><br>
-                    <span class="location-item"><img src="<?php echo get_template_directory_uri(); ?>/logo/logo-icon.png"><a onclick="myClick(14);" >Huntsville</a></span><br>
+                    
                   </div>
               </div>
           
